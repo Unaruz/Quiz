577243d8feb3445cb6f61fbc84b2d3fc6c719911
unaruz <unaruz@gmail.com>
2018-01-20 23:02:44 +0100

    Premier commit

---
+++ Test.java
@@ -0,0 +1,16 @@
+public class Test {
+
+	public static void main(String[] args) {
+		//C'est ici qu'on va écrire notre programme
+		// L'instruction qui affiche du texte sur l'écran 
+		// c'est:
+		// System.out.println(".....");
+		// System.out.print("....."); // mais ne retourne pas à la ligne
+		System.out.println("fatima" + " salah");
+		System.out.println("2" + " + " + "3");
+		//System.out.print(" ");
+		//System.out.println("salah");
+	}   
+	
+}
+	
